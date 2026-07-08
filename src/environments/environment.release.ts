541cba41eb741a93dcb84b28260f6961408c304d
naveenyadav9515 <naveenyadav9515@gmail.com>
2026-07-08 19:20:34 +0530

    chore: update google client ID and final render URLs

--- src/environments/environment.release.ts
+++ src/environments/environment.release.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://onespace-services-latest.onrender.com/api',
-  socketUrl: 'https://onespace-services-latest.onrender.com',
-  googleClientId: '305562630147-u7hnu7q3udsbmtag2cjd98mr53eq59am.apps.googleusercontent.com',
+  apiUrl: 'https://onespace-api-aqxi.onrender.com/api',
+  socketUrl: 'https://onespace-api-aqxi.onrender.com',
+  googleClientId: '778605648466-7k1na0aeuqid19p96b9h10nc5ctjf5lf.apps.googleusercontent.com',
   featureFlags: {
     enableMediaUploads: false, // Turned off globally for MVP
     enableAIInsights: true,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:5000/api',
   socketUrl: 'http://localhost:5000',
-  googleClientId: '305562630147-u7hnu7q3udsbmtag2cjd98mr53eq59am.apps.googleusercontent.com',
+  googleClientId: '778605648466-7k1na0aeuqid19p96b9h10nc5ctjf5lf.apps.googleusercontent.com',
   featureFlags: {
     enableMediaUploads: true,
     enableAIInsights: true,

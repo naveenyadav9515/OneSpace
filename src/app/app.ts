c0413432c4b6a3c7f71ca167728504ba4401a6de
naveenyadav9515 <naveenyadav9515@gmail.com>
2026-07-09 09:11:31 +0530

    feat: add SwUpdate to auto reload on new versions

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
 import { ToastComponent } from './shared/components/toast/toast.component';
 
 /**
@@ -13,4 +14,17 @@ import { ToastComponent } from './shared/components/toast/toast.component';
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [RouterOutlet, ToastComponent],
 })
-export class App {}
+export class App implements OnInit {
+  private swUpdate = inject(SwUpdate);
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates.subscribe((event) => {
+        if (event.type === 'VERSION_READY') {
+          // A new version is available, reload the page to apply it
+          window.location.reload();
+        }
+      });
+    }
+  }
+}
